96e831f85de2edfbff51bd04b3be269ae0e0c708
unknown <danielgiraldo9612@gmail.com>
2017-10-19 13:14:49 -0500

    post de personas

--- app.js
+++ app.js
@@ -7,10 +7,18 @@ var bodyParser = require('body-parser');
 var cors=require('cors');
 var mongoose = require('mongoose');
 var fs = require('fs');
+var Personas = require('./models/Persona');
 //var Ids=require('./routes/routing');
 //var index = require('./routes/index');
 //var users = require('./routes/users');
 
+//Estructura de Respuesta del Api
+var data;
+var response = {
+	"error" : "000",
+	"data" : data,
+	"errorMessage" : "Ok"
+}
 
 var app = express();
 
@@ -31,20 +39,39 @@ app.use(express.static(path.join(__dirname, 'public')));
 mongoose.connect('mongodb://localhost:27017/Bintec');
 
 //Load all models
-fs.readdirSync(__dirname + '/models').forEach(function (filename){
-	if(~filename.indexOf('.js')) require(__dirname + '/models/' + filename)
-});
+//fs.readdirSync(__dirname + '/models').forEach(function (filename){
+//	if(~filename.indexOf('.js')) require(__dirname + '/models/' + filename)
+//});
 
-app.get('/Persona', function(req, res){
-	mongoose.model('Personas').find(function(err, persona){
-		res.send(persona);
+app.get('/Persona/:idUsuario', function(req, res){
+	mongoose.model('Personas').find({_id : req.params.idUsuario},function(err, persona){
+		response.error = err;
+		response.data = persona;
+		if(err!=null){
+			response.errorMessage = "No se encontraron datos";
+		}
+		res.send(response);
 	});
 });
 
+app.post('/Persona',function(req, res){
+	let persona = new Personas();
+	persona.nombre = req.body.nombre;
+	persona.correo = req.body.correo;
+	persona.numCel = req.body.numCel;
+	persona.tipoDoc = req.body.tipoDoc;
+	persona.numDoc = req.body.numDoc;
+	persona.save(function(err, personaCreada){
+		response.error = err;
+		response.data = personaCreada;
+		if (err!=null){
+			response.errorMessage = "No se pudo Crear la Persona";
+		}
+		res.send(response);
+	});
 
-//app.use('/', index);
-//app.use('/users', users);
-//app.use('/routing',Ids);
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
 	var err = new Error('Not Found');
--- models/Persona.js
+++ models/Persona.js
@@ -3,18 +3,16 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var persona = new Schema({
-    Nombre: String,
-    Correo: String,
-    NumCel: String,
+    nombre: String,
+    correo: String,
+    numCel: String,
     tipoDoc: String,
     numDoc: String,
     cuentas : [{numCta : String, contra : String}],
     susActuales : [{idSus : { type: Schema.ObjectId, ref : 'Suscripciones'}}],
-
-
 },
 {
 	collection : "Personas"
 });
 // Compile model from schema
-mongoose.model('Personas', persona );
+module.exports = mongoose.model('Personas', persona );
